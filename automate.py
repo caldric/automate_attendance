f8952cf7ec53b8c7cc4ec491a7736161d29cfbba
caldric <cvaldric@gmail.com>
2020-07-31 09:53:06 -0400

    Auto format with Black

--- automate.py
+++ automate.py
@@ -12,43 +12,43 @@ max_seconds = 60 * max_mins
 random_seconds = randint(0, max_seconds)
 current_datetime = dt.now()
 start_datetime = current_datetime + timedelta(seconds=random_seconds)
-print(f'Current datetime: {current_datetime}')
-print(f'Start datetime: {start_datetime}')
+print(f"Current datetime: {current_datetime}")
+print(f"Start datetime: {start_datetime}")
 sleep(random_seconds)
 
 # Instantiate driver
-driver_path = '/usr/local/bin/chromedriver'
+driver_path = "/usr/local/bin/chromedriver"
 browser = webdriver.Chrome(driver_path)
 
 # Go to URL
-garnet_url = 'https://ga-garnet-production.herokuapp.com/sign_in'
+garnet_url = "https://ga-garnet-production.herokuapp.com/sign_in"
 browser.get(garnet_url)
 
 # Authenticate using GitHub
-github_button = browser.find_element_by_class_name('github')
+github_button = browser.find_element_by_class_name("github")
 github_button.click()
 
 # Login to GitHub
-if 'github.com' in browser.current_url:
-    user_field = browser.find_element_by_id('login_field')
-    password_field = browser.find_element_by_id('password')
-    gh_sign_in_button = browser.find_element_by_class_name('btn-primary')
+if "github.com" in browser.current_url:
+    user_field = browser.find_element_by_id("login_field")
+    password_field = browser.find_element_by_id("password")
+    gh_sign_in_button = browser.find_element_by_class_name("btn-primary")
 
     user_field.send_keys(config.user_name)
     password_field.send_keys(config.password)
     gh_sign_in_button.click()
 
 # Go to attendance page
-attendance_url = 'https://ga-garnet-production.herokuapp.com/memberships/3058'
+attendance_url = "https://ga-garnet-production.herokuapp.com/memberships/3058"
 browser.get(attendance_url)
 
 # Check in automatically (if button is present)
 try:
-    check_in_button = browser.find_element_by_tag_name('button')
+    check_in_button = browser.find_element_by_tag_name("button")
     check_in_button.click()
-    print('Successfully checked in!')
+    print("Successfully checked in!")
 except NoSuchElementException:
-    print('Unable to find check-in button.')
+    print("Unable to find check-in button.")
 
 # Quit browser afterwards
 sleep(10)
